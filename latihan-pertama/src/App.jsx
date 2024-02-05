bec374951aca1f021a8c091e23d316806c072cce
riyandarmawan <riyandarmawan042@gmail.com>
2024-02-05 13:05:15 +0700

    first commit

---
+++ latihan-pertama/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./component/Navbar"
+
+function App() {
+  return(
+    <>
+      <Navbar/>
+      <h1>Selamat datang, di pembelajaran React hari ini</h1>
+    </>
+  )
+}
+
+export default App
---
+++ latihan-pertama/src/component/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return(
+        <>
+            <h1>Navbar</h1>
+            <ul>
+                <li>Menu 1</li>
+                <li>Menu 2</li>
+                <li>Menu 3</li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar
